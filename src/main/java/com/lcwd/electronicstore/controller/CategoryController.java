c263a2ee601d3add954b81942ebc6bb13e238501
tejasbahiram123 <tejasbahiram12@gmail.com>
2023-07-20 14:09:53 +0530

    Logger implemented for Category module

--- src/main/java/com/lcwd/electronicstore/controller/CategoryController.java
+++ src/main/java/com/lcwd/electronicstore/controller/CategoryController.java
@@ -6,6 +6,8 @@ import com.lcwd.electronicstore.dto.PageableResponce;
 import com.lcwd.electronicstore.dto.ProductDto;
 import com.lcwd.electronicstore.service.CategoryService;
 import com.lcwd.electronicstore.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,16 +25,21 @@ public class CategoryController {
     @Autowired
     private ProductService productService;
 
+    private Logger logger= LoggerFactory.getLogger(CategoryController.class);
     //create
     @PostMapping("/create")
     public ResponseEntity<CategoryDto> createCategory(@Valid @RequestBody CategoryDto categoryDto) {
+        logger.info("start request for create Category {} ");
         CategoryDto category = categoryService.createCategory(categoryDto);
+        logger.info("complete request for create Category");
         return new ResponseEntity<>(category, HttpStatus.CREATED);
     }
 
     @PutMapping("/update/{categoryId}")
     public ResponseEntity<CategoryDto> updateCategory(@Valid @PathVariable String categoryId, @RequestBody CategoryDto categoryDto) {
+        logger.info("start request for update Category {},"+categoryId);
         CategoryDto updatedCategory = categoryService.updateCategory(categoryDto, categoryId);
+        logger.info("complete request for update Category {},"+categoryId);
         return new ResponseEntity<>(updatedCategory, HttpStatus.CREATED);
     }
 
@@ -43,20 +50,26 @@ public class CategoryController {
             @RequestParam(value = "sortBy", defaultValue = "title", required = false) String sortBy,
             @RequestParam(value = "sortDir", defaultValue = "ase", required = false) String sortDir
     ) {
+        logger.info("start request for getAll Category");
         PageableResponce<CategoryDto> allCategory = categoryService.getAllCategory(pageNumber, pageSize, sortBy, sortDir);
+        logger.info("complete request for getAll Category");
         return new ResponseEntity<PageableResponce<CategoryDto>>(allCategory, HttpStatus.FOUND);
     }
 
     @DeleteMapping("/{categoryId}")
     public ResponseEntity<ApiResponceMessage> deleteCategory(@PathVariable String categoryId) {
+        logger.info("start request for delete Category {},"+categoryId);
         categoryService.deleteCategory(categoryId);
         ApiResponceMessage responceMessage = ApiResponceMessage.builder().message("category deleted Successfully").success(true).status(HttpStatus.OK).build();
+        logger.info("complete request for delete Category {},"+categoryId);
         return new ResponseEntity<>(responceMessage, HttpStatus.OK);
     }
 
     @GetMapping("/{categoryId}")
     public ResponseEntity<CategoryDto> getCategory(@PathVariable String categoryId) {
+        logger.info("start request for get Category {},"+categoryId);
         CategoryDto category = categoryService.getCategory(categoryId);
+        logger.info("complete request for get Category {},"+categoryId);
         return new ResponseEntity<>(category, HttpStatus.FOUND);
 
     }
@@ -65,9 +78,9 @@ public class CategoryController {
     @PostMapping("/{categoryId}/products")
     public ResponseEntity<ProductDto> createProductWithCategory(@RequestBody ProductDto productDto,
                                                                 @PathVariable("categoryId") String categoryId) {
-
+        logger.info("start request for create product with  Category {},"+categoryId);
         ProductDto productWithCategory = productService.createWithCategory(productDto, categoryId);
-
+        logger.info("complete request for create product with Category {},"+categoryId);
         return new ResponseEntity<>(productWithCategory, HttpStatus.CREATED);
     }
 
@@ -76,7 +89,9 @@ public class CategoryController {
     public ResponseEntity<ProductDto> updateCategoryOfProduct(@RequestBody
                                                               @PathVariable String productId,
                                                               @PathVariable String categoryId) {
+        logger.info("start request for update Category of product {},"+categoryId);
         ProductDto productDto = productService.updateCategory(productId, categoryId);
+        logger.info("complete request for update Category of product {},"+categoryId);
         return new ResponseEntity<>(productDto, HttpStatus.OK);
 
     }
@@ -90,8 +105,9 @@ public class CategoryController {
             @RequestParam(value = "sortBy", defaultValue = "title", required = false) String sortBy,
             @RequestParam(value = "sortDir", defaultValue = "ase", required = false) String sortDir
     ) {
-
+        logger.info("start request for get products by Category {},"+categoryId);
         PageableResponce<ProductDto> responce = productService.getAllOfCategory(categoryId,pageNumber,pageSize,sortBy,sortDir);
+        logger.info("complete request for get products by Category {},"+categoryId);
         return new ResponseEntity<>(responce, HttpStatus.OK);
     }
 
--- src/main/java/com/lcwd/electronicstore/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/lcwd/electronicstore/service/impl/CategoryServiceImpl.java
@@ -8,10 +8,11 @@ import com.lcwd.electronicstore.helper.Helper;
 import com.lcwd.electronicstore.repository.CategoryRepository;
 import com.lcwd.electronicstore.service.CategoryService;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
@@ -25,24 +26,29 @@ public class CategoryServiceImpl implements CategoryService {
     @Autowired
     private ModelMapper mapper;
 
+    Logger logger= LoggerFactory.getLogger(CategoryServiceImpl.class);
+
     @Override
     public CategoryDto createCategory(CategoryDto categoryDto) {
-
+        logger.info("Initiating logic for create Category {} ");
         String randomId = UUID.randomUUID().toString();
         categoryDto.setCategoryId(randomId);
         Category category = this.mapper.map(categoryDto, Category.class);
         Category newCategory = this.categoryRepo.save(category);
+        logger.info("complete logic for create Category {} ");
         return this.mapper.map(newCategory, CategoryDto.class);
 
     }
 
     @Override
     public CategoryDto updateCategory(CategoryDto categoryDto, String categoryId) {
+        logger.info("Initiating logic for update Category {} ,"+categoryId);
         Category category = this.categoryRepo.findById(categoryId).orElseThrow(() -> new ResourceNotFoundException("category not found with this Id"));
         category.setTitle(categoryDto.getTitle());
         category.setDescription(categoryDto.getDescription());
         category.setCoverImage(categoryDto.getCoverImage());
         Category updated_category = this.categoryRepo.save(category);
+        logger.info("complete logic for update Category {} ,"+categoryId);
         return this.mapper.map(updated_category, CategoryDto.class);
 
     }
@@ -50,9 +56,11 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public PageableResponce<CategoryDto> getAllCategory(Integer pageNumber, Integer pageSize, String sortBy, String sortDir) {
 
+        logger.info("Initiating logic for getAll Category {} ,");
         Sort sort = (sortDir.equalsIgnoreCase("desc")) ? (Sort.by(sortBy).descending()) : (Sort.by(sortBy).ascending());
         PageRequest pageable = PageRequest.of(pageNumber, pageSize, sort);
         Page<Category> page = this.categoryRepo.findAll(pageable);
+        logger.info("complete logic for getAll Category {} ,");
         PageableResponce<CategoryDto> pageableResponce = Helper.getPageableResponce(page, CategoryDto.class);
 
         return pageableResponce;
@@ -60,14 +68,18 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public CategoryDto getCategory(String categoryId) {
+        logger.info("Initiating logic for get Category {} ,"+categoryId);
         Category category = this.categoryRepo.findById(categoryId).orElseThrow(() -> new ResourceNotFoundException("category not found with this Id.."));
+        logger.info("complete logic for get Category {} ,"+categoryId);
         return this.mapper.map(category, CategoryDto.class);
     }
 
     @Override
     public void deleteCategory(String categoryId) {
 
+        logger.info("Initiating logic for delete Category {} ,"+categoryId);
         Category category = this.categoryRepo.findById(categoryId).orElseThrow(() -> new ResourceNotFoundException("category not found with this Id"));
+        logger.info("complete logic for delete Category {} ,"+categoryId);
         this.categoryRepo.delete(category);
     }
 }
