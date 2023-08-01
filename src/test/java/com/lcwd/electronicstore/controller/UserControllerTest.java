37cb8d50b51d95cf0a9236bb30df40502b2db14c
tejasbahiram123 <tejasbahiram12@gmail.com>
2023-08-01 16:00:05 +0530

    update and getAllUser controller Unit Testing done

--- src/test/java/com/lcwd/electronicstore/controller/UserControllerTest.java
+++ src/test/java/com/lcwd/electronicstore/controller/UserControllerTest.java
@@ -1,6 +1,7 @@
 package com.lcwd.electronicstore.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lcwd.electronicstore.dto.PageableResponce;
 import com.lcwd.electronicstore.dto.UserDto;
 import com.lcwd.electronicstore.entity.User;
 import com.lcwd.electronicstore.service.UserService;
@@ -16,6 +17,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.Arrays;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -27,10 +29,8 @@ public class UserControllerTest {
 
     @MockBean
     private UserService userService;
-
     @Autowired
     private ModelMapper mapper;
-
     @Autowired
     private MockMvc mockMvc;
     private  User user;
@@ -58,16 +58,57 @@ public class UserControllerTest {
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.name").exists());
     }
+    @Test
+    public void updateUser() throws Exception {
+
+        String userId ="user123";
+        UserDto dto = mapper.map(user, UserDto.class);
+        Mockito.when(userService.updateUser(Mockito.any(),Mockito.anyString())).thenReturn(dto);
+
+        this.mockMvc.perform(MockMvcRequestBuilders.put("/users/"+userId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(convertObjectToJsonString(user))
+                .accept(MediaType.APPLICATION_JSON)
+        )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").exists());
+
+    }
+
     private String convertObjectToJsonString(Object user) {
-
         try{
-
     return new ObjectMapper().writeValueAsString(user);
-
         }catch (Exception e){
             e.printStackTrace();
             return null;
         }
     }
 
+    @Test
+    public void getAllUserTest() throws Exception {
+
+        UserDto object1 = UserDto.builder().name("tejas").email("tejas12@gmail.com").password("tejas2512").about("Testing").build();
+        UserDto object2 = UserDto.builder().name("aniket").email("tejas12@gmail.com").password("tejas2512").about("Testing").build();
+        UserDto object3 = UserDto.builder().name("sumit").email("tejas12@gmail.com").password("tejas2512").about("Testing").build();
+        UserDto object4 = UserDto.builder().name("elvish").email("tejas12@gmail.com").password("tejas2512").about("Testing").build();
+
+        PageableResponce<UserDto> pageableResponce= new PageableResponce<>();
+        pageableResponce.setContent(Arrays.asList(object1,object2,object3,object4));
+        pageableResponce.setPageNumber(10);
+        pageableResponce.setLastPage(false);
+        pageableResponce.setPageSize(100);
+        pageableResponce.setTotalElements(1000);
+
+        Mockito.when(userService.getAllUser(Mockito.anyInt(),Mockito.anyInt(),Mockito.anyString()
+                                            ,Mockito.anyString())).thenReturn(pageableResponce);
+    this.mockMvc.perform(
+            MockMvcRequestBuilders.get("/users")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON)
+    ).andDo(print())
+            .andExpect(status().isOk());
+
+
+    }
 }
