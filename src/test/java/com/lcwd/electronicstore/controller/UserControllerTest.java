acbe5e99773009da784cdfcf67799d1e3eac080d
tejasbahiram123 <tejasbahiram12@gmail.com>
2023-08-01 14:24:36 +0530

    CreateUser controller Unit Testing done

--- src/test/java/com/lcwd/electronicstore/controller/UserControllerTest.java
+++ src/test/java/com/lcwd/electronicstore/controller/UserControllerTest.java
@@ -1,4 +1,73 @@
 package com.lcwd.electronicstore.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lcwd.electronicstore.dto.UserDto;
+import com.lcwd.electronicstore.entity.User;
+import com.lcwd.electronicstore.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class UserControllerTest {
+
+    @MockBean
+    private UserService userService;
+
+    @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
+    private MockMvc mockMvc;
+    private  User user;
+    @BeforeEach
+    public  void init(){
+        user = User.builder()
+                .name("tejas")
+                .email("tejas12@gmail.com")
+                .about("this is")
+                .gender("male")
+                .imageName("abc.png").
+                password("tyfyt5554").build();
+    }
+    @Test
+    public  void createUserTest() throws Exception {
+
+        UserDto dto = mapper.map(user, UserDto.class);
+        Mockito.when(userService.createUser(Mockito.any())).thenReturn(dto);
+
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(convertObjectToJsonString(user))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name").exists());
+    }
+    private String convertObjectToJsonString(Object user) {
+
+        try{
+
+    return new ObjectMapper().writeValueAsString(user);
+
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 }
