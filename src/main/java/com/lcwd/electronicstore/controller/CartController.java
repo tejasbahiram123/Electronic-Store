ea6d17ce7b0fc05fc4b280ef39d12872de0ff532
tejasbahiram123 <tejasbahiram12@gmail.com>
2023-08-12 14:59:48 +0530

    CartController class created

---
+++ src/main/java/com/lcwd/electronicstore/controller/CartController.java
@@ -0,0 +1,9 @@
+package com.lcwd.electronicstore.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/carts")
+public class CartController {
+}
