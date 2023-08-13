bf23de4641fb54eea35436469b99d748f0184814
tejasbahiram123 <tejasbahiram12@gmail.com>
2023-08-13 11:58:02 +0530

    In CartController class addItemToCart method done

--- src/main/java/com/lcwd/electronicstore/controller/CartController.java
+++ src/main/java/com/lcwd/electronicstore/controller/CartController.java
@@ -1,9 +1,25 @@
 package com.lcwd.electronicstore.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.lcwd.electronicstore.dto.AddItemToCartRequest;
+import com.lcwd.electronicstore.dto.CartDto;
+import com.lcwd.electronicstore.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/carts")
 public class CartController {
+
+    @Autowired
+    private CartService cartService;
+    //add item to cart
+    @PostMapping("/{userId}")
+    public ResponseEntity<CartDto> addItemToCart(@PathVariable String userId, @RequestBody AddItemToCartRequest request){
+        CartDto cartDto = cartService.addItemToCart(userId, request);
+        return new ResponseEntity(cartDto, HttpStatus.OK);
+
+
+    }
 }
